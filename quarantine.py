fe56b867ca3c6e7c08aac1a5fa5485e204c9d7a4
adn888 <58361582+adn888@users.noreply.github.com>
2020-06-19 15:53:53 +1000

    Update quarantine.py

--- quarantine.py
+++ quarantine.py
@@ -10,8 +10,8 @@ url = "https://172.16.10.17/api/v1/fabric/virtual-machines?included_fields=tags&
 
 payload = {}
 headers = {
-  'Authorization': 'Basic YWRtaW46Vk13YXJlMSFWTXdhcmUxIQ==',
-  'Cookie': 'JSESSIONID=B3461C2F4C8D0D7E47AD00AF5DE2E60D'
+  'Authorization': 'Basic USERNAME',
+  'Cookie': 'PASSWORD'
 }
 
 response = requests.request("GET", url, headers=headers, data = payload, verify=False)
@@ -28,9 +28,9 @@ url = "https://172.16.10.17/policy/api/v1/infra/tags/tag-operations/"+id
 
 payload = "{\n  \"tag\": {\n    \"scope\": \"\",\n    \"tag\": \"quarantine\"\n   },\n  \"apply_to\": [\n    {\n         \"resource_type\": \"VirtualMachine\",\n         \"resource_ids\": [\n            \""+b+"\"\n          ]\n    }\n  ]\n}"
 headers = {
-  'Authorization': 'Basic YWRtaW46Vk13YXJlMSFWTXdhcmUxIQ==',
+  'Authorization': 'Basic USERNAME',
   'Content-Type': 'application/json',
-  'Cookie': 'JSESSIONID=691A435A96611A3D4BE58994BF3C1FC2'
+  'Cookie': 'PASSWORD'
 }
 
 response = requests.request("PUT", url, headers=headers, data = payload, verify=False)
